1b2e2df8cb4e8d26603bc8711f89495d7d37580f
Osiris Rosiles <daniel@rosiles.com.mx>
2016-10-26 23:39:07 -0500

    Cuenta Zungu maquetas

---
+++ ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Cambiar_contrasena.java
@@ -0,0 +1,13 @@
+package init_code.hyperion.zunguveterinarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cambiar_contrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar_contrasena);
+    }
+}
