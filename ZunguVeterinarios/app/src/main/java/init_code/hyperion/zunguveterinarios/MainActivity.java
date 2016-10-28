6bdd1a9831aa61450d1a89e09d45f03100fd7666
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-10-27 19:36:21 -0500

    Enlaces

--- ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/MainActivity.java
+++ ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
                 //Intent i = new Intent(MainActivity.this, Editar_establecimiento.class);
                 //Intent i = new Intent(MainActivity.this, Eliminar_cuenta.class);
                 //Intent i = new Intent(MainActivity.this, Cambiar_contrasena.class);
-                Intent i = new Intent(MainActivity.this, Preview_establecimiento.class);
+                //Intent i = new Intent(MainActivity.this, Preview_establecimiento.class);
+                Intent i = new Intent(MainActivity.this, Agregar_servicio.class);
                 //Intent i = new Intent(MainActivity.this, Agregar_servicio.class);
                 startActivity(i);
 
