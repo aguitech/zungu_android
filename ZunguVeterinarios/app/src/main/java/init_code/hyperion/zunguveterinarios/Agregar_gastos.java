0d08f2426aba9eff49da69b61c510628b060c0f7
Daniel Rosiles <daniel@rosiles.com.mx>
2016-10-28 04:51:36 -0500

    conexiones gastos

--- ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Agregar_gastos.java
+++ ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Agregar_gastos.java
@@ -1,7 +1,9 @@
 package init_code.hyperion.zunguveterinarios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Agregar_gastos extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class Agregar_gastos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_gastos);
+    }
+
+    public void goMenu(View v){
+        Intent i = new Intent(Agregar_gastos.this, Menu.class);
+        startActivity(i);
+    }
+
+    public void goGastos(View v){
+        Intent i = new Intent(Agregar_gastos.this, Gastos.class);
+        startActivity(i);
+    }
+
+    public void goAgregar(View v){
+        Intent i = new Intent(Agregar_gastos.this, Gastos.class);
+        startActivity(i);
+    }
+
+    public void goCancelar(View v){
+        Intent i = new Intent(Agregar_gastos.this, Gastos.class);
+        startActivity(i);
+    }
+
+    public void goBack(View v){
+        finish();
     }
 }
--- ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Gastos.java
+++ ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Gastos.java
@@ -1,7 +1,9 @@
 package init_code.hyperion.zunguveterinarios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Gastos extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class Gastos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gastos);
+    }
+
+    public void goMenu(View v){
+        Intent i = new Intent(Gastos.this, Menu.class);
+        startActivity(i);
+    }
+
+    public void goAgregar(View v){
+        Intent i = new Intent(Gastos.this, Agregar_gastos.class);
+        startActivity(i);
+    }
+
+    public void goBack(View v){
+        finish();
     }
 }
