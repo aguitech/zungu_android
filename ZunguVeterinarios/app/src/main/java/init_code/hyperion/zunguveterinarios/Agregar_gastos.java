7ca0c23d32e783ffb91433ccbc59de47e9839f8f
Rodolfo Ulises <rodolfoulises.ramirez@gmail.com>
2016-10-28 03:21:28 -0500

    Ultimas maquetas

---
+++ ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Agregar_gastos.java
@@ -0,0 +1,13 @@
+package init_code.hyperion.zunguveterinarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Agregar_gastos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_gastos);
+    }
+}
