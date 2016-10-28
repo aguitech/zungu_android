41a7182c5b3ed05b961253858657a09a1c513529
Osiris Rosiles <daniel@rosiles.com.mx>
2016-10-27 20:16:24 -0500

    manifest merge

---
+++ ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Citas.java
@@ -0,0 +1,13 @@
+package init_code.hyperion.zunguveterinarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Citas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_citas);
+    }
+}
