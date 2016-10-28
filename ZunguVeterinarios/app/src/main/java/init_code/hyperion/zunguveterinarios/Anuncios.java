a115c85eb893d7de7b1ca7dd0a37827e69f5df88
Daniel Rosiles <daniel@rosiles.com.mx>
2016-10-28 04:33:24 -0500

    conexiones anuncios

--- ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Anuncios.java
+++ ZunguVeterinarios/app/src/main/java/init_code/hyperion/zunguveterinarios/Anuncios.java
@@ -1,7 +1,9 @@
 package init_code.hyperion.zunguveterinarios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Anuncios extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class Anuncios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncios);
+    }
+
+    public void goMenu(View v){
+        Intent i = new Intent(Anuncios.this, Menu.class);
+        startActivity(i);
+    }
+
+    public void goActivos(View v){
+        Intent i = new Intent(Anuncios.this, Anuncios_activos.class);
+        startActivity(i);
+    }
+
+    public void goBack(View v){
+        finish();
     }
 }
